9e6fb88d43fd522ee6da34e94a3b6ac0ff3c37c6
Ramkumar <therealramkumar@gmail.com>
2024-05-30 17:33:36 +0530

    feat: Implement solution for LeetCode problem 125: Valid Palindrome

---
+++ src/125.valid-palindrome.rs
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=125 lang=rust
+ *
+ * [125] Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (47.65%)
+ * Likes:    9192
+ * Dislikes: 8324
+ * Total Accepted:    3M
+ * Total Submissions: 6.3M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * A phrase is a palindrome if, after converting all uppercase letters into
+ * lowercase letters and removing all non-alphanumeric characters, it reads the
+ * same forward and backward. Alphanumeric characters include letters and
+ * numbers.
+ * 
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = " "
+ * Output: true
+ * Explanation: s is an empty string "" after removing non-alphanumeric
+ * characters.
+ * Since an empty string reads the same forward and backward, it is a
+ * palindrome.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 2 * 10^5
+ * s consists only of printable ASCII characters.
+ * 
+ * 
+ */
+
+// @lc code=start
+impl Solution {
+    
+    pub fn is_palindrome(s: String) -> bool {
+        let s = s.chars().filter(|c| c.is_alphanumeric()).collect::<String>().to_lowercase();
+        (&s[(s.len()/2)+{if s.len()%2==0 {0} else {1}}..]).chars().rev().eq((&s[..s.len()/2]).chars())
+    }
+}
+// @lc code=end
+
