d4065d80de2d270f00108a9a2e4d8b3c5dbe31d1
Ramkumar <therealramkumar@gmail.com>
2024-05-31 21:25:20 +0530

    feat: Implement solution for LeetCode problem 226: Invert Binary Tree

---
+++ src/226.invert-binary-tree.rs
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=226 lang=rust
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (77.10%)
+ * Likes:    13847
+ * Dislikes: 220
+ * Total Accepted:    2.1M
+ * Total Submissions: 2.7M
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Given the root of a binary tree, invert the tree, and return its root.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+// @lc code=start
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut root_node = root.as_ref()?.borrow_mut();
+        (root_node.left, root_node.right) = (root_node.right.take(), root_node.left.take());
+        root_node.left = Self::invert_tree(root_node.left.take());
+        root_node.right = Self::invert_tree(root_node.right.take());
+        std::mem::drop(root_node);
+        root   
+    }
+}
+// @lc code=end
+
