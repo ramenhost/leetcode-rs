df32b5d519ab236a7f6a5a13913518bdae6001c6
Ramkumar <therealramkumar@gmail.com>
2024-05-31 16:21:07 +0530

    feat: Implement MinStack data structure for LeetCode problem 155

---
+++ src/155.min-stack.rs
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode id=155 lang=rust
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Medium (54.30%)
+ * Likes:    13936
+ * Dislikes: 866
+ * Total Accepted:    1.7M
+ * Total Submissions: 3.1M
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * Implement the MinStack class:
+ * 
+ * 
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
+ * 
+ * 
+ * You must implement a solution with O(1) time complexity for each
+ * function.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * 
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ * 
+ * Explanation
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin(); // return -3
+ * minStack.pop();
+ * minStack.top();    // return 0
+ * minStack.getMin(); // return -2
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -2^31 <= val <= 2^31 - 1
+ * Methods pop, top and getMin operations will always be called on non-empty
+ * stacks.
+ * At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
+ * 
+ * 
+ */
+
+// @lc code=start
+
+const MAX_CAPACITY: usize = 3 * 10^4;
+
+struct MinStack {
+    stack: Vec<i32>,
+    min_stack: Vec<i32>
+}
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MinStack {
+
+    fn new() -> Self {
+        MinStack {
+            stack: Vec::with_capacity(MAX_CAPACITY),
+            min_stack: Vec::with_capacity(MAX_CAPACITY)
+        }
+    }
+    
+    fn push(&mut self, val: i32) {
+        self.stack.push(val);
+        self.min_stack.push(i32::min(val, self.min_stack.last().cloned().unwrap_or(val)));
+    }
+    
+    fn pop(&mut self) {
+        self.min_stack.pop();
+        self.stack.pop();
+    }
+    
+    fn top(&self) -> i32 {
+        *self.stack.last().unwrap()
+    }
+    
+    fn get_min(&self) -> i32 {
+        *self.min_stack.last().unwrap()
+    }
+}
+
+// @lc code=end
+
